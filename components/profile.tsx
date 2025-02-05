25c3f738cce27295fccd19c27d881d91d6d8db75
compusophy <79760496+compusophy@users.noreply.github.com>
2025-02-05 13:28:03 -0700

    Update profile.tsx - remove email

--- components/profile.tsx
+++ components/profile.tsx
@@ -16,7 +16,7 @@ export default async function Profile() {
         href="/settings"
         className="flex w-full flex-1 items-center space-x-3 rounded-lg px-2 py-1.5 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
       >
-        <Image
+{/*         <Image
           src={
             session.user.image ??
             `https://avatar.vercel.sh/${session.user.email}`
@@ -25,7 +25,7 @@ export default async function Profile() {
           height={40}
           alt={session.user.name ?? "User avatar"}
           className="h-6 w-6 rounded-full"
-        />
+        /> */}
         <span className="truncate text-sm font-medium">
           {session.user.name}
         </span>
